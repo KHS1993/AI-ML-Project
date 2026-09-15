e91db802f9259f51e4c0c89a61e3b0c3efe93761
Chim-Cham <gustavfransson@hotmail.com>
2026-09-15 10:08:03 +0200

    database connection made, added python-dotenv and psycopg2-binary to requirments

--- database/FastAPI.py
+++ database/FastAPI.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from database.connection import SessionLocal, get_db
 
 app = FastAPI()
 
@@ -6,6 +10,17 @@ app = FastAPI()
 async def health_Check():
     return {"status": "Running"}
 
-@app.get("test-db")
+@app.get("/test-db")
 def test_database(db: Session = Depends(get_db)):
     return {"message": "Database connected!"}
+
+@app.get("/test-table")
+def get_table(db: Session = Depends(get_db)):
+    query = text("SELECT * FROM test")
+
+    result = db.execute(query)
+
+    table = [dict(row._mapping) for row in result]
+
+    return table
+
