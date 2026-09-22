6041021231af2ca36fae3776a67eebb97b5b970a
Samir <Samirpolozen@gmail.com>
2026-09-22 11:47:25 +0200

    Use enviroment variable for API URL

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from datetime import datetime
+import os
 
 import requests
 import streamlit as st
 
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000/predict",
+)
 
 
 # --------------------------------------------------
